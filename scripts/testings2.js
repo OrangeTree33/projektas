5aa46d315a8d311a2606898e5512fd8ec01f4f81
OrangeTree33 <95502332+OrangeTree33@users.noreply.github.com>
2021-12-16 20:37:38 +0200

    t

--- scripts/testings2.js
+++ scripts/testings2.js
@@ -5,3 +5,7 @@ function test(){
 function click(){
     document.getElementById("yes").style.color="red";
 }
+function num1(){
+    var p1= document.getElementById(one);
+    p1.insertAdjacentHtml(afterend,<div>dog</div>);
+}
